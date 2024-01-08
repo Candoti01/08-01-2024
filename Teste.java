bd521eae93ead5d6956931c19a74beb4fac8bd9c
Candoti01 <152418463+Candoti01@users.noreply.github.com>
2024-01-08 16:52:54 -0300

    Add files via upload

---
+++ Teste.java
@@ -0,0 +1,13 @@
+/**
+ * test
+ */
+public class Teste {
+    public static void main(String[] args) {
+        System.out.println(ImprimirTextoMaiusculo("HelLo WOrld " + (1 + 6 * 7 )));
+    }
+
+    public static String ImprimirTextoMaiusculo(String Texto) {
+        return Texto.toUpperCase();
+    }
+    
+}
