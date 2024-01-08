3e1547bab96137162e099198007aa93e4e894ec9
Candoti01 <152418463+Candoti01@users.noreply.github.com>
2024-01-08 17:19:37 -0300

    Add files via upload

---
+++ DoubleType.java
@@ -0,0 +1,7 @@
+public class DoubleType {
+    public static void main(String[] args) {
+       Double doubleVar = 9.999999999999999999999999999999999999999;
+       System.out.println(doubleVar);
+    }
+    
+}
