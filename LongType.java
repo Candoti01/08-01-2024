8d649680a488e8872e765accfa186267a9bd8355
Candoti01 <152418463+Candoti01@users.noreply.github.com>
2024-01-08 17:06:46 -0300

    Add files via upload

---
+++ LongType.java
@@ -0,0 +1,9 @@
+/**
+ * LongType
+ */
+public class LongType {
+    public static void main(String[] args) {
+        Long longVar = 999999999999999999L;
+        System.out.println(longVar);
+    }
+}
